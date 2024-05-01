c3282190867483701a3667575f8b5cfeeeaa8935
rhild1956 <rhild1956@westada.org>
2024-05-01 19:06:47 +0000

    started login easter egg; NOT FINISHED YET

---
+++ login/script.js
@@ -0,0 +1,13 @@
+function login(){
+    var user = document.getElementById("user");
+    localStorage.setItem("user", user);
+    var pass = document.getElementById("pass");
+    localStorage.setItem("pass", pass);
+
+    if localStorage.user = cyber {
+        
+    }
+}
+
+var button = document.getElementById("btn");
+button.addEventListener("click", login);
